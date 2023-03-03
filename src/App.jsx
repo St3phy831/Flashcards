009404ace6d547548a7af60f50de3991088925ab
Stephanie Hernandez <stephernandez831@gmail.com>
2023-03-02 23:15:31 -0800

    Able to view each flashcard

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from "./assets/react.svg";
 import Flashcard from "./components/Flashcard.jsx";
 import Header from "./components/Header.jsx";
 import { questions } from "./constants/Questions.jsx";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   const [isQuestion, setIsQuestion] = useState(true);
   const updateIsQuestion = () => setIsQuestion(!isQuestion);
+
+  const [ithQuestion, setIthQuestion] = useState(0);
+  const updateIthQuestion = () => {
+    setIsQuestion(true);
+    setIthQuestion((ithQuestion + 1) % 10);
+  };
 
   return (
     <div className="App">
@@ -18,8 +24,17 @@ function App() {
         description="How well do you know about US History? This will put you to the test!"
       />
       <h5>Number of Cards: 10</h5>
-      <Flashcard text={isQuestion ? questions[0].question: questions[0].answer} handlerFunction={updateIsQuestion}/>
-      <Button variant="primary">Next</Button>
+      <Flashcard
+        text={
+          isQuestion
+            ? questions[ithQuestion].question
+            : questions[ithQuestion].answer
+        }
+        handlerFunction={updateIsQuestion}
+      />
+      <Button variant="primary" onClick={updateIthQuestion}>
+        Next
+      </Button>
     </div>
   );
 }
